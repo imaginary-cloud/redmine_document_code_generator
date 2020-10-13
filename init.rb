6d1620417bf5b4b28f364a27bb9f1ac4f0785f09
Jorge Silva <dev.jorgemsilva@gmail.com>
2020-10-13 17:31:53 +0100

    configure options added.

--- init.rb
+++ init.rb
@@ -5,4 +5,6 @@ Redmine::Plugin.register :redmine_document_code_generator do
   version '0.0.1'
   url 'https://github.com/imaginary-cloud/redmine_document_code_generator'
   author_url 'https://imaginarycloud.com'
+  
+  settings default: {'empty' => true}, partial: 'settings/dummy'
 end
